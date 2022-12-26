c284b8db090d974d97535c3c31fe6e56c6138c85
Adiboy3112 <adiboy_3112@kgpian.iitkgp.ac.in>
2022-12-26 14:10:21 +0530

    parsing script made

--- chstone/adiboy_results/test.py
+++ chstone/adiboy_results/test.py
@@ -2,12 +2,16 @@ import re
 import sys
 
 def parse_perf_output(output: str, event: str) -> int:
-    match = re.search(r"\s*(\S+)\s+{event}".format(event=event), output)
-    if match:
-        value = match.group(1).replace(",", "")
-        return int(value)
+    if event=="branches":
+        match = re.findall(r"\s*([0-9,]*)\s+{event}".format(event=event), output)
+        value = match[1].replace(",", "")
+        return float(value)
+
     else:
-        return 0
+        match = re.search(r"\s*(\S+)\s+{event}".format(event=event), output)
+        value = match.group(1).replace(",", "")
+        return float(value)
+
 
 
 if len(sys.argv) < 2:
@@ -20,9 +24,19 @@ with open(filename, "r") as f:
     output = f.read()
 
 
-cpu_cycles = parse_perf_output(output, "cpu-cycles")
-print(f"CPU cycles: {cpu_cycles}")
+
+cpu_cycles = int(parse_perf_output(output, "cpu-cycles"))
+ref_cycles=int(parse_perf_output(output, "ref-cycles"))
+LLC_load_misses=int(parse_perf_output(output, "LLC-load-misses"))
+L1_dcache_load_misses=int(parse_perf_output(output, "L1-dcache-load-misses"))
+cache_misses=int(parse_perf_output(output, "cache-misses"))
+page_faults=int(parse_perf_output(output, "cache-misses"))
+instructions = int(parse_perf_output(output, "instructions"))
+branch_misses=int(parse_perf_output(output, "branch-misses"))
+branches=int(parse_perf_output(output, "branches"))
+run_time=1000*(parse_perf_output(output, "seconds time elapsed"))
 
 
-instructions = parse_perf_output(output, "instructions")
-print(f"Instructions: {instructions}")
+name, _ = filename.rsplit('.', 1)
+print(name,"|",run_time,"|",cpu_cycles,"|",instructions,"|",ref_cycles,"|",page_faults,"|",branches,"|",branch_misses,"|",LLC_load_misses,"|",L1_dcache_load_misses,"|",cache_misses)  
+
