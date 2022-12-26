399dc4b69469aaf68876458283188c5a7e5aad77
Adiboy3112 <adiboy_3112@kgpian.iitkgp.ac.in>
2022-12-26 14:17:02 +0530

    works perfectly the required format result

--- chstone/stat.sh
+++ chstone/stat.sh
@@ -11,8 +11,14 @@ while read -r folder; do
   gcc "$folder".c -o "$folder"
   chmod +x "$folder"
   sudo perf stat -e cpu-cycles,ref-cycles,LLC-load-misses,L1-dcache-load-misses,cache-misses -o "$folder".out ./"$folder"
-  sudo perf stat -e page-faults,instructions,branch-misses,branches  --append -o "$folder".out ./"$folder"
+  sudo perf stat -e page-faults,instructions,branch-misses,branches --append -o "$folder".out ./"$folder"
+  
+
   mv "$folder".out ../"$USER"_results
+
+  cd ..
+  cd "$USER"_results
+  python3 test.py "$folder".out >>results.out
   cd ..
   # Remove all files in the folder
 done < "$file"
