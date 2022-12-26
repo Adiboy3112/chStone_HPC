6bd028fcd23117bca5b41d69ac011ab5e72fd1fe
Adiboy3112 <adiboy_3112@kgpian.iitkgp.ac.in>
2022-12-27 01:54:10 +0530

    Parsed the output correctly

--- chstone/stat.sh
+++ chstone/stat.sh
@@ -5,6 +5,10 @@ file="input.txt"
 
 # Read the file containing the list of folder names
 mkdir "$USER"_results
+cp test.py "$USER"_results/
+echo "Workload|run-time|cpu-cycles|instructions|ref-cycles|page-faults|branches|branch-misses|LLC-load-misses|L1-dcache-load-misses|cache-misses">"$USER"_results/results.out
+echo "------------------------------------------------------------------------------------------------------------------------------------------">>"$USER"_results/results.out
+echo " ">>"$USER"_results/results.out
 while read -r folder; do
   # Change into the 
   cd "$folder"
---
+++ chstone/test.py
@@ -0,0 +1,42 @@
+import re
+import sys
+
+def parse_perf_output(output: str, event: str) -> int:
+    if event=="branches":
+        match = re.findall(r"\s*([0-9,]*)\s+{event}".format(event=event), output)
+        value = match[1].replace(",", "")
+        return float(value)
+
+    else:
+        match = re.search(r"\s*(\S+)\s+{event}".format(event=event), output)
+        value = match.group(1).replace(",", "")
+        return float(value)
+
+
+
+if len(sys.argv) < 2:
+    print("Error: No file name provided")
+    sys.exit(1)
+
+
+filename = sys.argv[1]
+with open(filename, "r") as f:
+    output = f.read()
+
+
+
+cpu_cycles = int(parse_perf_output(output, "cpu-cycles"))
+ref_cycles=int(parse_perf_output(output, "ref-cycles"))
+LLC_load_misses=int(parse_perf_output(output, "LLC-load-misses"))
+L1_dcache_load_misses=int(parse_perf_output(output, "L1-dcache-load-misses"))
+cache_misses=int(parse_perf_output(output, "cache-misses"))
+page_faults=int(parse_perf_output(output, "cache-misses"))
+instructions = int(parse_perf_output(output, "instructions"))
+branch_misses=int(parse_perf_output(output, "branch-misses"))
+branches=int(parse_perf_output(output, "branches"))
+run_time=1000*(parse_perf_output(output, "seconds time elapsed"))
+
+
+name, _ = filename.rsplit('.', 1)
+print(name,"|",run_time,"|",cpu_cycles,"|",instructions,"|",ref_cycles,"|",page_faults,"|",branches,"|",branch_misses,"|",LLC_load_misses,"|",L1_dcache_load_misses,"|",cache_misses)  
+
