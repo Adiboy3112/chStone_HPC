0d0796d6c9548ea795383e86b297e1e512e80bba
Adiboy3112 <adiboy_3112@kgpian.iitkgp.ac.in>
2022-12-28 12:10:27 +0530

    Made script tp run the test multiple times

--- chstone/stat.sh
+++ chstone/stat.sh
@@ -11,15 +11,17 @@ echo "--------------------------------------------------------------------------
 echo " ">>"$USER"_results/results.out
 while read -r folder; do
   # Change into the 
+  chmod +x run.sh
+  cp run.sh "$folder"/
   cd "$folder"
   gcc "$folder".c -o "$folder"
   chmod +x "$folder"
-  sudo perf stat -e cpu-cycles,ref-cycles,LLC-load-misses,L1-dcache-load-misses,cache-misses -o "$folder".out ./"$folder"
-  sudo perf stat -e page-faults,instructions,branch-misses,branches --append -o "$folder".out ./"$folder"
+  sudo perf stat -e cpu-cycles,ref-cycles,LLC-load-misses,L1-dcache-load-misses,cache-misses -o "$folder".out ./run.sh 2 "$folder"
+  sudo perf stat -e page-faults,instructions,branch-misses,branches --append -o "$folder".out ./run.sh 2 "$folder" 
   
 
   mv "$folder".out ../"$USER"_results
-
+  rm run.sh
   cd ..
   cd "$USER"_results
   python3 test.py "$folder".out >>results.out
